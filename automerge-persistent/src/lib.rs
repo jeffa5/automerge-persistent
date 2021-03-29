6ec2986d98118dbd9a16c027b8ae2da69fa5b23f
Andrew Jeffery <dev@jeffas.io>
2021-03-30 00:18:23 +0100

    Add persistent automerge initials

---
+++ automerge-persistent/src/lib.rs
@@ -0,0 +1,120 @@
+use std::{error::Error, fmt::Debug, rc::Rc};
+
+use automerge::Change;
+use automerge_backend::AutomergeError;
+use automerge_protocol::{ActorId, ChangeHash, Patch, UncompressedChange};
+
+pub trait Persister {
+    type Error: Error;
+
+    fn get_change(&self, actor_id: &ActorId, seq: u64) -> Result<Change, Self::Error>;
+
+    fn get_changes(&self) -> Result<Vec<Change>, Self::Error>;
+
+    fn insert_change(
+        &mut self,
+        actor_id: ActorId,
+        seq: u64,
+        change: Change,
+    ) -> Result<(), Self::Error>;
+
+    fn remove_change(&mut self, actor_id: &ActorId, seq: u64) -> Result<(), Self::Error>;
+
+    fn get_document(&self) -> Result<Vec<u8>, Self::Error>;
+
+    fn set_document(&mut self, data: Vec<u8>) -> Result<(), Self::Error>;
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum PersistentBackendError<E>
+where
+    E: Debug + Error + 'static,
+{
+    #[error(transparent)]
+    AutomergeError(#[from] AutomergeError),
+    #[error(transparent)]
+    PersisterError(E),
+}
+
+pub struct PersistentBackend<P: Persister> {
+    backend: automerge::Backend,
+    persister: P,
+}
+
+impl<P> PersistentBackend<P>
+where
+    P: Persister,
+{
+    pub fn new(persister: P) -> Self {
+        Self {
+            backend: automerge::Backend::init(),
+            persister,
+        }
+    }
+
+    pub fn load(persister: P) -> Result<Self, PersistentBackendError<P::Error>> {
+        let document = persister
+            .get_document()
+            .map_err(PersistentBackendError::PersisterError)?;
+        let changes = persister
+            .get_changes()
+            .map_err(PersistentBackendError::PersisterError)?;
+        let mut backend = automerge::Backend::load(document)?;
+        backend
+            .apply_changes(changes)
+            .map_err(PersistentBackendError::AutomergeError)?;
+        Ok(Self { backend, persister })
+    }
+
+    pub fn apply_changes(
+        &mut self,
+        changes: Vec<Change>,
+    ) -> Result<Patch, PersistentBackendError<P::Error>> {
+        for change in &changes {
+            self.persister
+                .insert_change(change.actor_id().clone(), change.seq, change.clone())
+                .map_err(PersistentBackendError::PersisterError)?;
+        }
+        self.backend
+            .apply_changes(changes)
+            .map_err(PersistentBackendError::AutomergeError)
+    }
+
+    pub fn get_heads(&self) -> Vec<ChangeHash> {
+        self.backend.get_heads()
+    }
+
+    pub fn apply_local_change(
+        &mut self,
+        change: UncompressedChange,
+    ) -> Result<(Patch, Rc<Change>), PersistentBackendError<P::Error>> {
+        let (patch, change) = self.backend.apply_local_change(change)?;
+        self.persister
+            .insert_change(change.actor_id().clone(), change.seq, (*change).clone())
+            .map_err(PersistentBackendError::PersisterError)?;
+        Ok((patch, change))
+    }
+
+    pub fn get_patch(&self) -> Result<Patch, PersistentBackendError<P::Error>> {
+        self.backend
+            .get_patch()
+            .map_err(PersistentBackendError::AutomergeError)
+    }
+
+    pub fn get_changes_for_actor_id(
+        &self,
+        actor_id: &ActorId,
+    ) -> Result<Vec<&Change>, PersistentBackendError<P::Error>> {
+        self.backend
+            .get_changes_for_actor_id(actor_id)
+            .map_err(PersistentBackendError::AutomergeError)
+    }
+
+    pub fn get_changes(&self, have_deps: &[ChangeHash]) -> Vec<&Change> {
+        self.backend.get_changes(have_deps)
+    }
+
+    pub fn get_missing_deps(&self) -> Vec<ChangeHash> {
+        self.backend.get_missing_deps()
+    }
+}
---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "automergeable";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            overlays = [ rust-overlay.overlay ];
+            system = system;
+          };
+          rust = pkgs.rust-bin.nightly.latest.rust;
+          cargoNix = pkgs.callPackage ./Cargo.nix { };
+        in
+        rec
+        {
+          packages = {
+            automergeable = cargoNix.workspaceMembers.automergeable.build;
+            automergeable-traits = cargoNix.workspaceMembers.automergeable-traits.build;
+            automergeable-derive = cargoNix.workspaceMembers.automergeable-derive.build;
+          };
+
+          defaultPackage = packages.automergeable;
+
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs;[
+              (rust.override {
+                extensions = [ "rust-src" "rustfmt" ];
+                targets = [ "wasm32-unknown-unknown" ];
+              })
+              cargo-edit
+              cargo-watch
+              cargo-udeps
+              cargo-expand
+              cargo-insta
+              crate2nix
+              rust-analyzer
+
+              wasm-pack
+              nodejs
+
+              rnix-lsp
+              nixpkgs-fmt
+            ];
+          };
+        }
+      );
+}
