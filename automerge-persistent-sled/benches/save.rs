25c7f0ee25868ab5d90708ea1f2a4c4ccdf0ee03
Andrew Jeffery <dev@jeffas.io>
2021-04-19 14:03:53 +0100

    Add flush benchmark

--- automerge-persistent-sled/benches/save.rs
+++ automerge-persistent-sled/benches/save.rs
@@ -32,6 +32,40 @@ fn small_backend_apply_local_change(c: &mut Criterion) {
     });
 }
 
+fn small_backend_apply_local_change_flush(c: &mut Criterion) {
+    c.bench_function("small backend apply local change flush", |b| {
+        b.iter_batched(
+            || {
+                let db = sled::Config::new().temporary(true).open().unwrap();
+                let sled = automerge_persistent_sled::SledPersister::new(
+                    db.open_tree("changes").unwrap(),
+                    db.open_tree("document").unwrap(),
+                    "".to_owned(),
+                );
+                let backend = automerge_persistent::PersistentBackend::load(sled).unwrap();
+                let mut frontend = automerge::Frontend::new();
+                let ((), change) = frontend
+                    .change::<_, _, InvalidChangeRequest>(None, |doc| {
+                        doc.add_change(LocalChange::set(
+                            Path::root().key("a"),
+                            Value::Primitive(Primitive::Str("abcdef".to_owned())),
+                        ))
+                        .unwrap();
+                        Ok(())
+                    })
+                    .unwrap();
+
+                (db, backend, change.unwrap())
+            },
+            |(db, mut persistent_doc, change)| {
+                persistent_doc.apply_local_change(change).unwrap();
+                db.flush().unwrap()
+            },
+            criterion::BatchSize::SmallInput,
+        )
+    });
+}
+
 fn small_backend_apply_changes(c: &mut Criterion) {
     c.bench_function("small backend apply changes", |b| {
         b.iter_batched(
@@ -103,6 +137,6 @@ fn small_backend_compact(c: &mut Criterion) {
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(50);
-    targets = small_backend_apply_local_change, small_backend_apply_changes, small_backend_compact
+    targets = small_backend_apply_local_change, small_backend_apply_local_change_flush, small_backend_apply_changes, small_backend_compact
 }
 criterion_main!(benches);
